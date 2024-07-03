0bfa8765ae8c36a471ff6766f33a082528cd3b69
dered-dev <jlcc170@gmail.com>
2024-07-02 19:56:31 -0600

    ADD cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+const cors = require('cors'); // Import the cors package
+
 
 
 var app = express();
-
-
+app.use(cors()); 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 
 app.get(
