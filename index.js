ed1ab84c81ffa9b30a573576b05bc776da710b30
dered-dev <jlcc170@gmail.com>
2024-07-02 23:55:21 -0600

    Clean code

--- index.js
+++ index.js
@@ -1,61 +1,55 @@
-var express = require('express');
-var bodyParser = require("body-parser");
-const cors = require('cors'); // Import the cors package
+var express = require('express')
+var bodyParser = require("body-parser")
+const cors = require('cors')
 
+var app = express()
+app.use( cors() )
 
-
-var app = express();
-app.use(cors()); 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
+app.use( bodyParser.urlencoded({ extended: false }) )
+app.use( bodyParser.json() )
 
 app.get(
     '/',
-    (request, response) => {
-        response.send('pagina Principal')
+    (req, res) => {
+        res.send('pagina Principal')
     }
-);
+)
 
-
-app.post('/', function (req, res) {
+app.post('/', function (req, req) {
     var email = req.body.email
 
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "Bearer " + process.env.HSAPIKEY);
+    const myHeaders = new Headers()
+    myHeaders.append("Content-Type", "application/json")
+    myHeaders.append("Authorization", "Bearer " + process.env.HSAPIKEY)
 
     const raw = JSON.stringify({
-    "filters": [
-        {
+    "filters": [{
         "propertyName": "email",
         "operator": "EQ",
         "value": email
-        }
-    ]
-    });
+        }]
+    })
 
     const requestOptions = {
     method: "POST",
     headers: myHeaders,
     body: raw,
     redirect: "follow"
-    };
+    }
 
     fetch("https://api.hubapi.com/crm/v3/objects/contacts/search", requestOptions)
     .then((response) => response.text())
     .then((result) => {
         res.end(result)
     })
-    .catch((error) => console.error(error));
+    .catch((error) => console.error(error))
     
-});
+})
 
 app.listen(
     3000,
     () => {
-        console.log('Servidor corriendo')
+        console.log('Server listen in port 3000')
     }
 )
 
