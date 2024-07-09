51c4b3756f503a7ea4a39a1b4891af8b0c83ff4e
dered-dev <jlcc170@gmail.com>
2024-07-09 16:40:05 -0600

    ADD certifier endpoint

--- index.js
+++ index.js
@@ -46,6 +46,48 @@ app.post('/', function (req, res) {
     
 })
 
+
+// certifier
+app.post('/certifier', function (req, res) {
+    const { name, email, issueDate, expiryDate, mentor } = req.body
+
+    const myHeaders = new fetch.Headers()
+    myHeaders.append("Certifier-Version", "2022-10-26")
+    myHeaders.append("accept", "application/json")
+    myHeaders.append("authorization", "Bearer " + process.env.CERTTOKEN)
+    myHeaders.append("content-type", "application/json")
+
+    const raw = JSON.stringify({
+        "recipient": {
+            "name": name,
+            "email": email
+        },
+        "issueDate": issueDate,
+        "expiryDate": expiryDate,
+        "customAttributes": {
+            "custom.mentor": mentor
+        }
+    })
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    }
+
+    fetch("https://api.certifier.io/v1/credentials/create-issue-send", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+            res.json(result)
+        })
+        .catch((error) => {
+            console.error(error)
+            res.status(500).send('Error processing request')
+        })
+})
+
+// listen port
 app.listen(
     3000,
     () => {
