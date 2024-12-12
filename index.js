64bd947b4a6145a434d73cf507ad177577d6d455
dered-dev <jlcc170@gmail.com>
2024-12-12 16:52:41 -0600

    FIX certifier POST
    Signed-off-by: dered-dev <jlcc170@gmail.com>

--- index.js
+++ index.js
@@ -1,90 +1,111 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
-require('dotenv').config()
+var express = require("express")
+var bodyParser = require("body-parser")
+const cors = require("cors")
+const axios = require("axios")
+require("dotenv").config()
 
-
-var app = express();
+var app = express()
 const corsOptions = {
-    origin: 'https://buoyant.io', 
-    optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+  origin: "https://buoyant.io",
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.get('/', (request, response) => {
-    response.send('pagina Principal');
-});
+app.get("/", (request, response) => {
+  response.send("pagina Principal")
+})
 
-app.post('/', function (req, res) {
-    var email = req.body.email
+app.post("/", function (req, res) {
+  var email = req.body.email
 
-    const myHeaders = new Headers()
-    myHeaders.append("Content-Type", "application/json")
-    myHeaders.append("Authorization", "Bearer " + process.env.HSAPIKEY)
+  const myHeaders = new Headers()
+  myHeaders.append("Content-Type", "application/json")
+  myHeaders.append("Authorization", "Bearer " + process.env.HSAPIKEY)
 
-    const raw = JSON.stringify({
-    "filters": [{
-        "propertyName": "email",
-        "operator": "EQ",
-        "value": email
-        }]
-    })
+  const raw = JSON.stringify({
+    filters: [
+      {
+        propertyName: "email",
+        operator: "EQ",
+        value: email
+      }
+    ]
+  })
 
-    const requestOptions = {
+  const requestOptions = {
     method: "POST",
     headers: myHeaders,
     body: raw,
     redirect: "follow"
-    }
+  }
 
-    fetch("https://api.hubapi.com/crm/v3/objects/contacts/search", requestOptions)
+  fetch("https://api.hubapi.com/crm/v3/objects/contacts/search", requestOptions)
     .then((response) => response.text())
     .then((result) => {
-        res.end(result)
+      res.end(result)
     })
     .catch((error) => console.error(error))
-    
 })
 
 // certifier
-app.post('/certifier', function (req, res) {
-    const { name, email, issueDate, expiryDate, groupId } = req.body;
-
-    axios
+app.post("/certifier", function (req, res) {
+  const { name, email, issueDate, expiryDate, groupId } = req.body
+  console.log(req.body)
+  axios
     .request({
-        method: 'POST',
-        url: 'https://api.certifier.io/v1/credentials/create-issue-send',
-        headers: {
-          accept: 'application/json',
-          'Certifier-Version': '2022-10-26',
-          'content-type': 'application/json',
-          authorization: 'Bearer '+ process.env.CERTTOKEN
+      method: "POST",
+      url: "https://api.certifier.io/v1/credentials/create-issue-send",
+      headers: {
+        accept: "application/json",
+        "Certifier-Version": "2022-10-26",
+        "content-type": "application/json",
+        authorization: "Bearer " + process.env.CERTTOKEN
+      },
+      data: {
+        recipient: {
+          name: name,
+          email: email,
         },
-        data: {
-            recipient: {
-                name: name,
-                email: email
-            },
-            issueDate: issueDate,
-            expiryDate: expiryDate,
-            groupId: groupId
-        }
+        issueDate: issueDate,
+        expiryDate: expiryDate,
+        groupId: groupId
+      }
     })
     .then(({ data }) => res.json(data))
-    .catch(err => {
-        console.error(err)
-        res.status(500).send('Error processing request')
+    .catch((err) => {
+      console.error(err)
+      res.status(500).send("Error processing request")
     })
 
+//   const urlCertifier = "https://api.certifier.io/v1/credentials/create-issue-send"
+//   const optionsCertifier = {
+//     method: "POST",
+//     headers: {
+//       accept: "application/json",
+//       "Certifier-Version": "2022-10-26",
+//       "content-type": "application/json",
+//       "authorization": "Bearer " + process.env.CERTTOKEN
+//     },
+//     body: JSON.stringify({
+//       recipient: {
+//         name: name,
+//         email: email
+//       },
+//       issueDate: issueDate,
+//       expiryDate: expiryDate,
+//       groupId: groupId
+//     })
+//   }
+
+//   fetch(urlCertifier, optionsCertifier)
+//     .then((res) => res.json())
+//     .then((json) => json)
+//     .catch((err) => console.error(err))
 })
 
-app.listen(
-    3000,
-    () => {
-        console.log('Server listen in port 3000')
-    }
-)
+app.listen(3000, () => {
+  console.log("Server listen in port 3000")
+})
